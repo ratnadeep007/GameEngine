51b29c0f92d5acc027c7e30c56e1c1c377a2dd1b
ratnadeep007 <ratnadeepofghato@gmail.com>
2019-04-11 13:44:59 +0530

    Layer and layerstack added
    Re-generated project to use /MDd (mutlthreading dll) instead  /MTd

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,31 @@
 #include <Hazel.h>
 
+class ExmapleLayer : public Hazel::Layer
+{
+public:
+	ExmapleLayer()
+		: Layer("Example")
+	{
+
+	}
+
+	void OnUpdate() override
+	{
+		HZ_INFO("ExampleLayer::Update");
+	}
+
+	void OnEvent(Hazel::Event& event) override
+	{
+		HZ_TRACE("{0}", event);
+	}
+};
+
 class Sandbox : public Hazel::Application
 {
 public:
 	Sandbox()
 	{
-
+		PushLayer(new ExmapleLayer());
 	}
 	~Sandbox()
 	{
--- premake5.lua
+++ premake5.lua
@@ -64,14 +64,17 @@ project "Hazel"
 
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
+		buildoptions "/MDd"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
+		buildoptions "/MDd"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "HZ_DIST"
+		buildoptions "/MDd"
 		optimize "On"
 
 project "Sandbox"
@@ -111,12 +114,15 @@ project "Sandbox"
 
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
+		buildoptions "/MDd"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
+		buildoptions "/MDd"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "HZ_DIST"
+		buildoptions "/MDd"
 		optimize "On"
